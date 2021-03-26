77e34b1d5d17dd61ca39ba862a6201f8a5c3bd41
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-03-26 18:37:36 +0300

    .

---
+++ ClassPerson/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPerson
+{
+    class Manager : Employee
+    {
+        public List<Worker> ListOfWorkers = new List<Worker>(){};
+
+        public void Dismiss(Worker worker)
+        {
+            ListOfWorkers.Remove(worker);
+        }
+
+        public void Hire(Worker worker)
+        {
+            ListOfWorkers.Add(worker);
+        }
+
+        public void SetSalary(Manager manager, double salary)
+        {
+            foreach (Worker worker in ListOfWorkers)
+            {
+                if (worker.Salary >= manager.Salary)
+                {
+                    Console.WriteLine("Increase salary");
+                }
+                else
+                {
+                    manager.Salary = salary;
+                }
+            }
+        }
+    }
+}
--- ClassPerson/Program.cs
+++ ClassPerson/Program.cs
@@ -10,6 +10,37 @@ namespace ClassPerson
     {
         static void Main(string[] args)
         {
+            Worker worker1 = new Worker();
+            Worker worker2 = new Worker();
+            Worker worker3 = new Worker();
+
+            Manager manager = new Manager();
+
+            worker1.Name = "Mr.Ivan";
+            worker1.Salary = 100;
+            worker1.Age = 30;
+            worker1.Seniority = 1;
+
+            worker2.Name = "Mr.Oleg";
+            worker2.Salary = 150;
+            worker2.Age = 20;
+            worker2.Seniority = 2;
+
+            worker3.Name = "Mr.Vasya";
+            worker3.Salary = 100;
+            worker3.Age = 30;
+            worker3.Seniority = 1;
+
+            manager.Name = "Mr.Boss";
+            manager.SetSalary(manager, 200);
+            manager.Age = 40;
+            manager.Seniority = 10;
+
+            manager.Hire(worker1);
+            manager.Hire(worker2);
+            manager.Hire(worker3);
+
+            manager.Dismiss(worker2);
         }
     }
 }
